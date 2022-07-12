d796c4f592282144b55fab2812d154dd10fdc102
Jeffry1990 <109137757+Jeffry1990@users.noreply.github.com>
2022-07-12 13:08:47 +0300

    Add files via upload

---
+++ дз1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace дз1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            int age;
+            string zodiacSign;
+            string placeOfWork;
+
+            Console.Write("Как вас зовут: ");
+            name = Console.ReadLine();
+            Console.Write("Сколько вам лет: ");
+            age = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Кто вы по знаку зодиака: ");
+            zodiacSign = Console.ReadLine();
+            Console.Write("Где вы работаете: ");
+            placeOfWork = Console.ReadLine();
+
+            Console.WriteLine("Вас зовут "+name+", вам "+age+"год, вы "+zodiacSign+" и работаете "+ placeOfWork + ".");
+        }
+    }
+}
